a7417b7ced28472f34ac8b83cca9756995f7f108
andkon <andrew.konoff@gmail.com>
2014-11-17 13:50:06 -0800

    Added iOS

---
+++ iOS/Chainlock/Chainlock/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Chainlock
+//
+//  Created by AK on 2014-11-17.
+//  Copyright (c) 2014 Chainlock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS/Chainlock/Chainlock/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Chainlock
+//
+//  Created by AK on 2014-11-17.
+//  Copyright (c) 2014 Chainlock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
